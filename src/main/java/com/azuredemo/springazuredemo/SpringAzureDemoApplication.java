f66e67dcb9f3ebcc1cc6c886aa62f4739a1fbf38
jay <jayshah5109@gmail.com>
2023-09-03 16:44:04 +0530

    initial commit for azure deployment

---
+++ src/main/java/com/azuredemo/springazuredemo/SpringAzureDemoApplication.java
@@ -0,0 +1,23 @@
+package com.azuredemo.springazuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAzureDemoApplication {
+
+	@GetMapping("/message/{name}")
+	public String message(@PathVariable String name) {
+		String msg = "This application has been hosted over Azure by: " + name;
+		return msg;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemoApplication.class, args);
+	}
+
+}
